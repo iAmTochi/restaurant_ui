cafcd04a37631e2a51e7d0683fa2cb858de53b9a
Tochukwu Ugwu <ugwukelvintochukwu@gmail.com>
2022-02-09 21:44:35 +0100

    Update order.dart

---
+++ lib/models/order.dart
@@ -0,0 +1,11 @@
+import 'package:restaurant_ui/models/food.dart';
+import 'package:restaurant_ui/models/restuarant.dart';
+
+class Order {
+  Restaurant restaurant;
+  Food food;
+  String date;
+  int quantity;
+
+  Order({this.restaurant, this.food, this.date, this.quantity});
+}
